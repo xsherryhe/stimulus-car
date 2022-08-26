333c2267d99d6f00626ab7a980385c38e4e9142c
Sherry He <xsherryhe@gmail.com>
2022-08-26 17:52:04 -0400

    Completed edits for using Stimulus to delete and add variants fields

--- app/javascript/controllers/variants_form_controller.js
+++ app/javascript/controllers/variants_form_controller.js
@@ -1,62 +1,85 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static values = { index: Number };
-  static targets = ["container", "fieldSet"];
+  static values = { newIndex: Number };
+  static targets = ["container", "fieldSet", "heading"];
+
+  connect() {
+    this.#numberHeadings();
+  }
 
   addVariant(e) {
     e.preventDefault();
 
     const fieldSet = document.createElement('div');
     fieldSet.setAttribute('data-variants-form-target', 'fieldSet');
-    fieldSet.appendChild(this.#createIndexDisplay());
+    fieldSet.setAttribute('data-index', this.newIndexValue);
+    fieldSet.appendChild(this.#createHeading());
     ['name', 'color'].forEach(attribute => fieldSet.appendChild(this.#createField(attribute)));
+    fieldSet.appendChild(this.#createDeleteLink());
     this.containerTarget.appendChild(fieldSet);
-    this.indexValue++;
+    this.newIndexValue++;
+    this.#numberHeadings();
   }
 
   deleteVariant(e) {
     e.preventDefault();
 
-    this.indexValue--;
-    this.containerTarget.removeChild(this.fieldSetTargets.slice(-1)[0]);
-    this.containerTarget.appendChild(this.#createInput('_destroy', 'hidden', 1));
+    const fieldSet = e.target.parentNode.parentNode;
+    this.containerTarget.removeChild(fieldSet);
+    this.containerTarget.appendChild(this.#createInput('_destroy', fieldSet.getAttribute('data-index'), 'hidden', 1));
+    this.#numberHeadings();
   }
 
-  #createIndexDisplay() {
-    const indexDisplay = document.createElement('h5');
-    indexDisplay.textContent = this.#indexText();
-    return indexDisplay;
+  #createHeading() {
+    const heading = document.createElement('h5');
+    heading.setAttribute('data-variants-form-target', 'heading');
+    heading.textContent = 'Variant';
+    return heading;
   }
 
-  #createField(attribute) {
+  #createField(attribute, variant_index = this.newIndexValue) {
     const field = document.createElement('div');
-    field.appendChild(this.#createLabel(attribute));
-    field.appendChild(this.#createInput(attribute));
+    field.classList.add('field');
+    field.appendChild(this.#createLabel(attribute, variant_index));
+    field.appendChild(this.#createInput(attribute, variant_index));
     return field;
   }
 
-  #createLabel(attribute) {
+  #createLabel(attribute, variant_index) {
     const label = document.createElement('label');
-    label.setAttribute('for', this.#attributeId(attribute))
+    label.setAttribute('for', this.#attributeId(attribute, variant_index))
     label.textContent = attribute[0].toUpperCase() + attribute.slice(1);
     return label;
   }
 
-  #createInput(attribute, type = 'text', value = null) {
+  #createInput(attribute, variant_index, type = 'text', value = null) {
     const input = document.createElement('input');
     input.setAttribute('type', type);
-    input.setAttribute('id', this.#attributeId(attribute));
-    input.setAttribute('name', `car[variants_attributes][${this.indexValue}][${attribute}]`);
+    input.setAttribute('id', this.#attributeId(attribute, variant_index));
+    input.setAttribute('name', `car[variants_attributes][${variant_index}][${attribute}]`);
     if(value) input.setAttribute('value', value);
     return input;
   }
 
-  #attributeId(attribute) {
-    return `car_variants_attributes_${this.indexValue}_${attribute}`;
+  #createDeleteLink() {
+    const linkDiv = document.createElement('div'),
+          link = document.createElement('a');
+
+    link.setAttribute('data-action', 'variants-form#deleteVariant');
+    link.setAttribute('href', '#');
+    link.textContent = 'Delete This Variant';
+
+    linkDiv.appendChild(link)
+    return linkDiv;
   }
 
-  #indexText() {
-    return 'Variant # ' + (this.indexValue + 1);
+  #attributeId(attribute, variant_index) {
+    return `car_variants_attributes_${variant_index}_${attribute}`;
+  }
+
+  #numberHeadings() {
+    this.headingTargets.forEach((heading, i) => 
+      heading.textContent = 'Variant # ' + (i + 1));
   }
 }
